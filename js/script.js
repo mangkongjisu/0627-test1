5f799bb54c418b68a30ff4bfc36ae2c438255092
mangkongjisu <061299@naver.com>
2024-06-27 09:56:39 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+setTimeout(function(){$('.box_wrap').isotope()},100);
+
+$('.box_wrap').isotope({
+    // options
+    itemSelector: '.box',
+    layoutMode: 'masonry'
+  });
+
+  $(".btns>li").click(function(){
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+    const i=$(this).attr("data-filter") //속성값 메서드
+    $('.box_wrap').isotope({filter:i})
+
+  })
+
+  $(".fancybox").fancybox();
+  
